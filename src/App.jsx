6b05a22cbc43999c7d96098a688e1a26cc9b8248
BrandonBravoduoc <bra.bravol@duocuc.cl>
2025-12-09 00:06:40 -0300

    xd

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import AuthLayout from "./components/layouts/AuthLayout";
 import Login from "./pages/auth/Login";
 import HomePage from "./pages/Home";
 import Register from "./pages/auth/Register";
+import Profile from "./pages/user/Profile";
 
 const App = () => {
     return (
         <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/profile" element={<Profile />} />
             <Route path="/" element={<HomePage />} /> 
         </Routes>
     );
