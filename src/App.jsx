131f2f0b87f2236fb8a84b294e51f64d596df997
Deybi-hud <hu4nt4r@gmail.com>
2025-12-09 00:10:50 -0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./pages/auth/Login";
 import HomePage from "./pages/Home";
 import Register from "./pages/auth/Register";
-import Profile from "./pages/user/Profile";
+import ProductDetail from "./pages/ProductDetail";
 
 const App = () => {
     return (
         <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/" element={<HomePage />} /> 
+            <Route path="/product/:id" element={<ProductDetail />} />
+            <Route path="/" element={<HomePage />} />
+
         </Routes>
     );
 };
