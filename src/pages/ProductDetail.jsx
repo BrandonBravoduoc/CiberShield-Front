ffdfd2b697758288e7bb9f92ff22be9a8d03e7dd
Balthier <bra.bravol@duocuc.cl>
2026-03-12 09:01:28 -0300

    Import useCart from CartContext in ProductDetail

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import ProductService from '../services/product/ProductService'; //
 import ProductDetailTemplate from '../components/templates/ProductDetailTemplate';
+import { useCart } from '../context/CartContext';
 
 const ProductDetail = () => {
   const { id } = useParams(); 
